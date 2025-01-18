6f7de180e84e9b7717659012a54042c4bb26489a
ER28 <l.guerra-rago@pm.me>
2025-01-19 00:16:05 +0100

    add todo

--- example/src/user/user_controller.py
+++ example/src/user/user_controller.py
@@ -9,13 +9,17 @@ from example.src.user.user_service import UserService
 @Controller()
 class UserController:
     inject = ['UserService']
+    # Todo: refactor to remove this attribute
     api: Any
+    # Todo: refactor to remove this attribute
     routes: List[Dict[str, Any]] = []
 
     def __init__(self, user_service: UserService):
         self.user_service = user_service
+        # Todo: refactor to remove this call
         self.register_routes()
 
+    # Todo: refactor to remove this method
     def register_routes(self):
         for attr_name in dir(self):
             attr = getattr(self, attr_name)
@@ -25,7 +29,6 @@ class UserController:
 
     @endpoint(path="/user")
     def create_user_route(self, req):
-        # self will now be properly available here
         self.user_service.create_user("John Doe")
         return Response(
             status_code=200,
